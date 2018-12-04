5098e99a9c053868645b6c04a7b70b64812a25b3
godhyo <pkpk5087@naver.com>
2018-12-04 21:12:00 +0900

    test

--- app/src/main/java/moapp/knu/moapp/MainActivity.java
+++ app/src/main/java/moapp/knu/moapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //adsf
-        //test
+        //testddddd
         setContentView(R.layout.activity_main);
         System.out.println("test");
 
