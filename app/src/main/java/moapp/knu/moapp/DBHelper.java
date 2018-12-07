fcdb5179924326a28787d35405defbb159bcca1a
sangmin95 <sangmin95@gmail.com>
2018-12-07 11:23:52 +0900

    db update success

--- app/src/main/java/moapp/knu/moapp/DBHelper.java
+++ app/src/main/java/moapp/knu/moapp/DBHelper.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 import android.widget.Toast;
 
 public class DBHelper extends SQLiteOpenHelper {
 
     private Context context;
+
+    public static String TAG_d = "sangmin DBHelper";
 
     public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
         super(context,name,factory,version);
@@ -20,7 +23,7 @@ public class DBHelper extends SQLiteOpenHelper {
         StringBuffer sb = new StringBuffer();
         sb.append("CREATE TABLE IF NOT EXISTS illnese(");
         sb.append("ill_name TEXT PRIMARY KEY, ");
-        sb.append("mandatory INTERGER NOT NULL, ");
+        sb.append("mandatory INTERGER UNIQUE, ");
         sb.append("vaccin INTEGER DEFAULT 0, ");
         sb.append("vaccin_date TEXT, ");
         sb.append("next_date TEXT default '9999-12-31')");
@@ -38,13 +41,14 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public void addIllnese(Illnese illnese){
+        Log.d(TAG_d,"addIllnese"+illnese.getName());
         SQLiteDatabase illdb = getWritableDatabase();
 
         String sql = new String();
-        sql = "INSERT INTO illnese (ill_name, mandatory) VALUES (" + "'"+illnese.getName()+"'"+","+illnese.getMandatory()+");";
+        sql = "INSERT OR REPLACE INTO illnese (ill_name, mandatory) VALUES (" + "'"+illnese.getName()+"'"+","+illnese.getMandatory()+");";
         Toast.makeText(context,sql,Toast.LENGTH_LONG).show();
-        //Cursor cursor = illdb.rawQuery(sql,null);
-        //cursor.close();
+        Cursor cursor = illdb.rawQuery(sql,null);
+        cursor.close();
         illdb.execSQL(sql);
         //Toast.makeText(context,illnese.getName()+" Insert 완료",Toast.LENGTH_SHORT).show();
     }
--- app/src/main/java/moapp/knu/moapp/MainActivity.java
+++ app/src/main/java/moapp/knu/moapp/MainActivity.java
@@ -5,14 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
     Button btnMoveillness;
+    static final String TAG_d = "sangmin main";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG_d,"onCreate");
         btnMoveillness  = (Button)findViewById(R.id.button_illnese);
 
         btnMoveillness.setOnClickListener(new View.OnClickListener(){
