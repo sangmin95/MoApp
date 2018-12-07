6c2d1fe036814f695354a80afe74a7127cc4022b
sangmin <sangmin95@gmail.com>
2018-12-07 09:29:56 +0900

    sangmin comment

---
+++ app/src/main/java/moapp/knu/moapp/DBHelper.java
@@ -0,0 +1,51 @@
+package moapp.knu.moapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private Context context;
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context,name,factory,version);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase illdb){
+        StringBuffer sb = new StringBuffer();
+        sb.append("CREATE TABLE IF NOT EXISTS illnese(");
+        sb.append("ill_name TEXT PRIMARY KEY, ");
+        sb.append("mandatory INTERGER NOT NULL, ");
+        sb.append("vaccin INTEGER DEFAULT 0, ");
+        sb.append("vaccin_date TEXT, ");
+        sb.append("next_date TEXT default '9999-12-31')");
+        illdb.execSQL(sb.toString());
+        Toast.makeText(context,sb.toString(),Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Toast.makeText(context, "버전이 올라갔습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void testDB(){
+        SQLiteDatabase illdb = getReadableDatabase();
+    }
+
+    public void addIllnese(Illnese illnese){
+        SQLiteDatabase illdb = getWritableDatabase();
+
+        String sql = new String();
+        sql = "INSERT INTO illnese (ill_name, mandatory) VALUES (" + "'"+illnese.getName()+"'"+","+illnese.getMandatory()+");";
+        Toast.makeText(context,sql,Toast.LENGTH_LONG).show();
+        //Cursor cursor = illdb.rawQuery(sql,null);
+        //cursor.close();
+        illdb.execSQL(sql);
+        //Toast.makeText(context,illnese.getName()+" Insert 완료",Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/moapp/knu/moapp/MainActivity.java
+++ app/src/main/java/moapp/knu/moapp/MainActivity.java
@@ -1,17 +1,28 @@
 package moapp.knu.moapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnMoveillness;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //adsf
-        //test
         setContentView(R.layout.activity_main);
-        System.out.println("test");
+        btnMoveillness  = (Button)findViewById(R.id.button_illnese);
 
+        btnMoveillness.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        alarm_info.class);
+                startActivity(intent);
+            }
+        });
     }
 }
