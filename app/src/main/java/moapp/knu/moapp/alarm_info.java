a90dc68f35ecfae641b87754ba893904ccce5cd2
sangmin <sangmin95@gmail.com>
2018-12-07 09:38:28 +0900

    alarm_info.java

---
+++ app/src/main/java/moapp/knu/moapp/alarm_info.java
@@ -0,0 +1,29 @@
+package moapp.knu.moapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alarm_info extends AppCompatActivity {
+    private  DBHelper dbHelper;
+    Illnese Corona = new Illnese("코로나 바이러스",1);
+    Illnese kennel = new Illnese("켄넬코프",1);
+    Illnese Dirofilaria = new Illnese("심장사상충",0);
+    Illnese rabies = new Illnese("광견병",1);
+    Illnese influenza = new Illnese("인플루엔자",0);
+    Illnese DHPPL = new Illnese("종합백신",1);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_info);
+        //illdb 생성
+        dbHelper = new DBHelper(alarm_info.this,"illdb",null,1);
+        dbHelper.addIllnese(Corona);
+        //dbHelper.addIllnese(kennel);
+        //dbHelper.addIllnese(Dirofilaria);
+        //dbHelper.addIllnese(rabies);
+        //dbHelper.addIllnese(influenza);
+        //dbHelper.addIllnese(DHPPL);
+        //dbHelper.testDB();
+    }
+}
