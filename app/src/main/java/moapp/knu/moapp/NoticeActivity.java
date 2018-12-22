13fb842d9d9f0bee8bc974904cc20164d659b7d2
harryborison <steve6238@naver.com>
2018-12-22 15:57:15 +0900

    df

--- app/src/main/java/moapp/knu/moapp/NoticeActivity.java
+++ app/src/main/java/moapp/knu/moapp/NoticeActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 public class NoticeActivity extends AppCompatActivity {
 //강아지 정보 알림
     private  DBHelper dbHelper;
+  /*
     static final String TAG_d = "sangmin alarm_info";
     Illnese Corona = new Illnese("코로나 바이러스",1);
     Illnese kennel = new Illnese("켄넬코프",1);
@@ -16,25 +17,36 @@ public class NoticeActivity extends AppCompatActivity {
     Illnese rabies = new Illnese("광견병",1);
     Illnese influenza = new Illnese("인플루엔자",0);
     Illnese DHPPL = new Illnese("종합백신",1);
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
-
-        Log.d(TAG_d,"onCreate");
-        //illdb 생성
-        dbHelper = new DBHelper(NoticeActivity.this,"illdb",null,1);
-        dbHelper.addIllnese(Corona);
-        dbHelper.addIllnese(kennel);
-        dbHelper.addIllnese(Dirofilaria);
-        dbHelper.addIllnese(rabies);
-        dbHelper.addIllnese(influenza);
-        dbHelper.addIllnese(DHPPL);
-        dbHelper.testDB();
-
         getWindow().setWindowAnimations(0);
+
+
+
     }
+    public void on_click1(View v)
+    {
+        Log.d("asdfasdf","adfklajfklajfklawjfklawjefklasjkfl");
+        Log.d("asdfasdf","adfklajfklajfklawjfklawjefklasjkfl");
+        Intent intent1 = new Intent(this, Click1.class);
+        Log.d("asdfasdf","adfklajfklajfklawjfklawjefklasjkfl");
+        Log.d("asdfasdf","adfklajfklajfklawjfklawjefklasjkfl");
+        Log.d("asdfasdf","adfklajfklajfklawjfklawjefklasjkfl");
+        startActivity(intent1);
+    }
+
+
+
+    public void on_click3(View v)
+    {
+
+        Intent intent1 = new Intent(this, Click3.class);
+        startActivity(intent1);
+    }
+
 
     public void careClick(View v){
         Intent intent1 = new Intent(this, CareActivity.class);
